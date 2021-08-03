95822b0c27259ee583433e5cc03923a79d5445a9
eduardonk9999 <thiago.novak7@gmail.com>
2021-08-03 11:14:36 -0300

    Add, estado no componente

--- ignite/Chapter-I/01-github-explore/src/App.jsx
+++ ignite/Chapter-I/01-github-explore/src/App.jsx
@@ -1,6 +1,12 @@
 import './styles/global.css'
 
 import { RepositoryList } from './components/RepositoryList'
+import { Counter } from './components/Counter'
 export function App() {
-  return <RepositoryList />
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  )
 }
--- ignite/Chapter-I/01-github-explore/src/components/RepositoryList.jsx
+++ ignite/Chapter-I/01-github-explore/src/components/RepositoryList.jsx
@@ -1,30 +1,17 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/unform/unform'
+}
 export function RepositoryList() {
   return (
     <section className="repository">
       <h1>Lista de repositório</h1>
-
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">
-            Acessar repositórios
-          </a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">
-            Acessar repositórios
-          </a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">
-            Acessar repositórios
-          </a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   )
